6e8e381d9724cfbf554257a7b71280e3593d11b3
Raghav Saini <raghavsaini30795@gmail.com>
2023-03-12 09:53:23 +0530

    add web server

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -79,7 +79,7 @@ function Form() {
         loader(messageDiv)
 
         // fetch data from the server
-        const response = await fetch("http://localhost:5000", {
+        const response = await fetch("https://beyondgpt.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
